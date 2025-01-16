667070fa58c9f6b9294d55f307d22d637bee44e0
parhamf6 <parhamfdev@proton.me>
2025-01-16 17:00:00 +0330

    quera questions.

---
+++ questions/QueraTests/CinemaCapcity.py
@@ -0,0 +1,9 @@
+# خانه ی دوست
+# https://quera.org/problemset/176782
+user = list(input().split())
+if user[0]>user[1]:
+    print(user[0])
+if user[0]<user[1]:
+    print(user[1])
+if user[0]==user[1]:
+    print(user[0])
---
+++ questions/QueraTests/CodeClass.py
@@ -0,0 +1,8 @@
+# کلاس کد
+# https://quera.org/problemset/66864
+n=5000
+numberlist = ""
+for i in range(1,n+1):
+    numberlist+=str(i)
+k = int(input())
+print(numberlist[k-1])
